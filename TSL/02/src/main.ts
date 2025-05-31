44a62eda0b4866531d0f1d8b4a584b08a1fd1d58
Nimira43 <n.rabone72@aol.com>
2025-05-31 07:30:23 +0100

    Radar waves

--- TSL/02/src/main.ts
+++ TSL/02/src/main.ts
@@ -1,10 +1,10 @@
 import './style.css'
 import * as THREE from 'three/webgpu'
-import { Fn, positionLocal } from 'three/tsl'
+import { abs, clamp, cos, dot, Fn, length, max, mix, mod, negate, oneMinus, positionLocal, sin, smoothstep, step, time, vec2, vec3,
+} from 'three/tsl'
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js'
 
 const scene = new THREE.Scene()
-
 const camera = new THREE.PerspectiveCamera(
   75,
   window.innerWidth / window.innerHeight,
@@ -17,7 +17,6 @@ const renderer = new THREE.WebGPURenderer()
 renderer.setSize(window.innerWidth, window.innerHeight)
 document.body.appendChild(renderer.domElement)
 renderer.setAnimationLoop(animate)
-
 window.addEventListener('resize', function () {
   camera.aspect = window.innerWidth / window.innerHeight
   camera.updateProjectionMatrix()
@@ -27,19 +26,101 @@ window.addEventListener('resize', function () {
 const controls = new OrbitControls(camera, renderer.domElement)
 controls.enableDamping = true
 
+//@ts-ignore
+const Line = Fn(([position, direction, distance, thickness]) => {
+  const projection = dot(position, direction) 
+  const clampedProjection = clamp(projection, 0.0, distance)
+  const line = smoothstep(
+    thickness,
+    0.0,
+    length(position.sub(clampedProjection.mul(direction)))
+  )
+  return line
+})
+
+//@ts-ignore
+const Circle = Fn(([position, radius, thickness]) => {
+  const distance = length(position).sub(radius)
+  return smoothstep(thickness, 0, abs(distance))
+})
+
 const main = Fn(() => {
   const p = positionLocal.toVar()
-  return p
+  p.mulAssign(2)
+
+  const radius = 0.5
+  const thickness = 0.02
+
+  const circleOffset = vec2(-0.66, 0.66)
+  const circle = Circle(p.sub(circleOffset), radius, thickness)
+
+  const angle = time
+  const direction = vec2(cos(angle), sin(angle))
+
+  const radiusLine = Line(p.sub(circleOffset), direction, radius, thickness)
+  const radiusEndPosition = direction.mul(radius)
+
+  const frequency = Math.PI * 2
+
+  const sineWave = smoothstep(
+    thickness,
+    0,
+    abs(
+      p.y
+        .sub(circleOffset.y)
+        .sub(sin(p.x.mul(frequency).sub(time).sub(Math.PI)).mul(0.5))
+    )
+  ) // offset it
+  sineWave.assign(step(0, p.x).mul(sineWave)) 
+
+  const cosineWave = smoothstep(
+    thickness,
+    0,
+    abs(
+      p.x.sub(circleOffset.x).sub(cos(p.y.mul(frequency).add(time)).mul(0.5))
+    )
+  ) // offset it
+  cosineWave.assign(step(p.y, 0).mul(cosineWave)) 
+
+  const sineReferenceLine = Line(
+    p.sub(circleOffset).sub(radiusEndPosition),
+    vec2(1, 0),
+    negate(radiusEndPosition.x).sub(circleOffset.x),
+    thickness
+  )
+  const cosineReferenceLine = Line(
+    p.sub(circleOffset).sub(radiusEndPosition),
+    vec2(0, -1),
+    radiusEndPosition.y.add(circleOffset.y),
+    thickness
+  )
+
+  const dottedSineReferenceLine = sineReferenceLine.mul(
+    step(0.5, mod(p.x.mul(10), 1))
+  )
+
+  const dottedCosineReferenceLine = cosineReferenceLine.mul(
+    step(0.5, mod(p.y.mul(10), 1))
+  )
+
+  const finalColour = mix(circle, vec3(1, 1, 0), radiusLine)
+  finalColour.assign(mix(finalColour, vec3(1, 0, 0), sineWave))
+  finalColour.assign(mix(finalColour, vec3(0, 1, 0), cosineWave))
+  finalColour.assign(mix(finalColour, vec3(1, 1, 0), dottedSineReferenceLine))
+  finalColour.assign(
+    mix(finalColour, vec3(1, 1, 0), dottedCosineReferenceLine)
+  )
+
+  return finalColour
 })
 
 const material = new THREE.NodeMaterial()
-material.fragmentNode = positionLocal.length().mul(15).fract().step(0.5)
+material.fragmentNode = main()
 
-const mesh = new THREE.Mesh(new THREE.BoxGeometry(), material)
+const mesh = new THREE.Mesh(new THREE.PlaneGeometry(2, 2), material)
 scene.add(mesh)
 
 function animate() {
   controls.update()
   renderer.render(scene, camera)
 }
-
